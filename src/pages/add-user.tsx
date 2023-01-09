a67d00771447ec08509f31c0024dbd3f83b0d0a5
Rafael Dias <rafaelldiias100@gmail.com>
2023-01-08 22:57:58 -0300

    feat: navegação para página principal ao cadastrar usuário

--- src/pages/add-user.tsx
+++ src/pages/add-user.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { ChangeEvent, FormEvent, useState } from "react";
 import Button from "../components/Button";
 import TextField from "../components/TextField";
 
 const AddUser: NextPage = () => {
+  const router = useRouter();
   const [values, setValues] = useState({
     name: "",
     email: "",
@@ -17,6 +19,7 @@ const AddUser: NextPage = () => {
   function handleAddUser(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     console.log(values);
+    router.push("/");
   }
 
   return (
