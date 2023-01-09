cc08d00e34e304ae05f23f74f226fd68aab29d37
Rafael Dias <rafaelldiias100@gmail.com>
2023-01-08 22:25:54 -0300

    fix: correção no estilo do background do botão

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,7 +7,7 @@ const Button = ({ onClick, children }: Props) => {
   return (
     <button
       onClick={onClick}
-      className="bg=indigo-600 text-white py-2 px-6 my-10 rounded hover:bg-indigo-700"
+      className="bg-indigo-600 text-white py-2 px-6 my-10 rounded hover:bg-indigo-700"
     >
       {children}
     </button>
