993190ccfab8ea8a126c9f390156fcdbc7acbe3b
Rafael Dias <rafaelldiias100@gmail.com>
2023-01-08 22:30:06 -0300

    refactor: mudando layout das páginas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className="container mx-auto px-2 max-w-5xl pt-10 md:pt-32">
+      <h1 className="text-center font-bold text-2xl text-gray-700">
+        CRUD com Redux Toolkit
+      </h1>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,12 +10,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container mx-auto px-2 max-w-5xl pt-10 md:pt-32">
-        <h1 className="text-center font-bold text-2xl text-gray-700">
-          CRUD com Redux Toolkit
-        </h1>
-        <UserList />
-      </main>
+      <UserList />
     </>
   );
 };
